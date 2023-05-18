847e139f21330793e581b4201c742dfe5739b050
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-05-18 18:22:27 +0000

    commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Array of possible "who" options
+  let who = ["The cat", "My grandma", "His turtle", "My bird"];
+
+  // Array of possible "action" options
+  let action = ["ate", "peed", "crushed", "broke"];
+
+  // Array of possible "what" options
+  let what = ["my homework", "the keys", "the car"];
+
+  // Array of possible "when" options
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  // Function to generate a random excuse
+  function generateExcuse() {
+    let randomWho = who[Math.floor(Math.random() * who.length)];
+    let randomAction = action[Math.floor(Math.random() * action.length)];
+    let randomWhat = what[Math.floor(Math.random() * what.length)];
+    let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+    let excuse = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}`;
+    return excuse;
+  }
+
+  // Get the excuse element by its id
+  let excuseElement = document.getElementById("excuse");
+
+  // Set the innerHTML of the excuse element to a randomly generated excuse
+  excuseElement.innerHTML = generateExcuse();
 };
